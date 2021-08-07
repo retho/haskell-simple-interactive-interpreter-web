876f0de53767595926a1fd05b7dd2db9cf8ed94c
Safonov Evgeny <safonov.e.s@gmail.com>
2021-08-07 22:15:38 +0300

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# * https://blog.bloomca.me/2017/12/15/how-to-push-folder-to-github-pages.html
+
+rm -rf gh-pages-dist
+cp .dist gh-pages-dist
+cd gh-pages-dist && rm cli web && mv web.html index.html
+git init && git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/retho/haskell-simple-interactive-interpreter-web.git
+git push --force origin master:gh-pages
